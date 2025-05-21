71c983bfaa4aedb1822eb6b3b9631d0acfd93b67
DoubleVV35 <virgile.villard@viacesi.fr>
2025-05-21 18:39:28 +0200

    Mono-Instance Software

--- EasySave/EasySave by ProSoft/App.xaml.cs
+++ EasySave/EasySave by ProSoft/App.xaml.cs
@@ -1,17 +1,30 @@
 ﻿using EasySave_by_ProSoft.Properties; // For Settings.Default
 using System.Globalization;
 using System.Windows;
+using System.Threading;
 
 namespace EasySave_by_ProSoft
 {
     public partial class App : System.Windows.Application
     {
         private const string DefaultCultureName = "en-US"; // Default language if nothing is saved or if error
+        private static Mutex? _mutex;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             string initialThreadCultureName = Thread.CurrentThread.CurrentUICulture.Name;
             string initialSavedLang = Settings.Default.UserLanguage;
+
+            const string mutexName = "CryptoSoft_MonoInstance_Mutex";
+            bool createdNew;
+            _mutex = new Mutex(true, mutexName, out createdNew);
+            if (!createdNew)
+            {
+                // Another instance is already running, show a message and exit
+                System.Windows.MessageBox.Show("Another instance of the application is already running.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                Environment.Exit(0);
+            }
+
 
             string savedLang = Settings.Default.UserLanguage;
             CultureInfo targetCulture;
@@ -44,6 +57,13 @@ namespace EasySave_by_ProSoft
 
             base.OnStartup(e);
         }
+        protected override void OnExit(ExitEventArgs e)
+        {
+            _mutex?.ReleaseMutex();
+            _mutex?.Dispose();
+            base.OnExit(e);
+        }
+
 
         private void ApplyCulture(CultureInfo culture)
         {
