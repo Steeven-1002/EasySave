5bbe88bad61f0bfe14c91ea64906f5c8a4da00e1
DoubleVV35 <virgile.villard@viacesi.fr>
2025-05-27 14:33:19 +0200

    Concurrential logs

--- DLL/LoggingLibrary/LoggingLibrary/LogService.cs
+++ DLL/LoggingLibrary/LoggingLibrary/LogService.cs
@@ -3,20 +3,21 @@
 namespace LoggingLibrary
 {
     /// <summary>
-    /// Provides logging services for saving log entries to a file.
+    /// Fournit des services de journalisation pour enregistrer des entrées de log dans un fichier.
     /// </summary>
     public class LogService
     {
         private readonly LogFile _logFile;
         private readonly ILogFormatter _logFormatter;
         private readonly string _logDirectoryPath;
+        private readonly object _logWriteLock = new();
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="LogService"/> class.
+        /// Initialise une nouvelle instance de la classe <see cref="LogService"/>.
         /// </summary>
-        /// <param name="logDirectoryPath">The directory path where log files will be stored.</param>
-        /// <param name="logFormatter">The formatter used to format log entries.</param>
-        /// <exception cref="ArgumentNullException">Thrown if <paramref name="logFormatter"/> is null.</exception>
+        /// <param name="logDirectoryPath">Le chemin du dossier où les fichiers de log seront stockés.</param>
+        /// <param name="logFormatter">Le formateur utilisé pour formater les entrées de log.</param>
+        /// <exception cref="ArgumentNullException">Déclenchée si <paramref name="logFormatter"/> est null.</exception>
         public LogService(string logDirectoryPath, ILogFormatter logFormatter)
         {
             _logDirectoryPath = logDirectoryPath;
@@ -25,17 +26,25 @@ namespace LoggingLibrary
         }
 
         /// <summary>
-        /// Logs an entry with the specified details.
+        /// Journalise une entrée avec les détails spécifiés.
         /// </summary>
-        /// <param name="timestamp">The timestamp of the log entry.</param>
-        /// <param name="saveName">The name of the save operation.</param>
-        /// <param name="sourcePath">The source file path in UNC format.</param>
-        /// <param name="targetPath">The target file path in UNC format.</param>
-        /// <param name="fileSize">The size of the file being logged, in bytes. Optional.</param>
-        /// <param name="durationMs">The duration of the file transfer, in milliseconds. Optional.</param>
-        /// <param name="encryptionTimeMs">The duration of the encryption process, in milliseconds. Optional.</param>
-        /// <param name="details">Additional details about the operation. Optional.</param>
-        public void Log(DateTime timestamp, string saveName, string sourcePath, string targetPath, long? fileSize = null, double? durationMs = null, double? encryptionTimeMs = null, string? details = null)
+        /// <param name="timestamp">L'horodatage de l'entrée de log.</param>
+        /// <param name="saveName">Le nom de l'opération de sauvegarde.</param>
+        /// <param name="sourcePath">Le chemin source du fichier au format UNC.</param>
+        /// <param name="targetPath">Le chemin cible du fichier au format UNC.</param>
+        /// <param name="fileSize">La taille du fichier journalisé, en octets. Optionnel.</param>
+        /// <param name="durationMs">La durée du transfert de fichier, en millisecondes. Optionnel.</param>
+        /// <param name="encryptionTimeMs">La durée du chiffrement, en millisecondes. Optionnel.</param>
+        /// <param name="details">Détails supplémentaires sur l'opération. Optionnel.</param>
+        public void Log(
+            DateTime timestamp,
+            string saveName,
+            string sourcePath,
+            string targetPath,
+            long? fileSize = null,
+            double? durationMs = null,
+            double? encryptionTimeMs = null,
+            string? details = null)
         {
             var logEntry = new LogEntry
             {
@@ -50,13 +59,17 @@ namespace LoggingLibrary
             };
 
             string formattedLog = _logFormatter.FormatLog(logEntry);
-            _logFile.WriteLogEntry(formattedLog);
+
+            lock (_logWriteLock)
+            {
+                _logFile.WriteLogEntry(formattedLog);
+            }
         }
 
         /// <summary>
-        /// Gets the directory path where log files are stored.
+        /// Retourne le chemin du dossier où les fichiers de log sont stockés.
         /// </summary>
-        /// <returns>The directory path as a string.</returns>
+        /// <returns>Le chemin du dossier sous forme de chaîne.</returns>
         public string GetlogDirectoryPath()
         {
             return _logDirectoryPath;
